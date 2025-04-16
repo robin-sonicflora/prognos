67438b9233b9599d97fa95cdf55f98f294b74121
robin-sonicflora <robin.jansson@sonicflora.com>
2025-04-16 23:39:28 +0200

    Update sonicflora_forecast.py

--- sonicflora_forecast.py
+++ sonicflora_forecast.py
@@ -80,7 +80,7 @@ editable_skord_data = st.data_editor(
     disabled=["Land", "Grundintäkt (kr/m²)", "Intäkt för Sonicflora per m² (kr)"]
 )
 editable_skord_data["Grundintäkt (kr/m²)"] = editable_skord_data["Skörd (kg/m²)"] * editable_skord_data["Pris (kr/kg)"]
-editable_skord_data["Intäkt för Sonicflora per m² (kr)"] = editable_skord_data["Grundintäkt (kr/m²)"] * (1 + skordeokning / 100) * (andel_sonicflora / 100)
+editable_skord_data["Intäkt för Sonicflora per m² (kr)"] = editable_skord_data["Grundintäkt (kr/m²)"] * (skordeokning / 100) * (andel_sonicflora / 100)
 
 
 # Standarddata för redigering
